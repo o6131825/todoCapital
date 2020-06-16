ccbd9ab134d6772c5209dbe88c80ab797398c17c
o6131825 <o6131825@gmail.com>
2020-06-16 17:11:30 +0300

    sectrt si

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   db: 'mongodb+srv://admin:admin@cluster0-bvbe8.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  secret:'06131825o6131825Af06131825'
+  secret:'some_itproger'
 }
